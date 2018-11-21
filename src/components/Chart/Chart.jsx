24ab91aa048a96db12ed487e60b4916190fd6e39
ArtemK <artem.korchunof@gmail.com>
2018-11-21 03:54:31 +0200

    make chart wrap

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import { useAsync } from "react-use";
 
+import { Device } from "../../services/API";
 import DashboardWrap from "../DashboardWrap";
 import "./Chart.scss";
 
-function Device({ history }) {
+function Chart({ history }) {
+  const { loading, value } = useAsync(Device.get);
+  const [options, setOptions] = useState([]);
+  const [pickedSuggest, setPickedSuggest] = useState(null);
+
+  useEffect(
+    () => {
+      if (!value) return;
+      setOptions(
+        value.data.data.map(item => ({
+          value: item.id,
+          label: item.name
+        }))
+      );
+    },
+    [loading]
+  );
   return (
-    <DashboardWrap headlineTitle="Chart" contentComponent={<div>Chart</div>} />
+    <DashboardWrap
+      headlineTitle="Chart"
+      contentComponent={
+        !pickedSuggest ? (
+          <div className="content-wrap content-wrap__title content-wrap__center">
+            <span className="title-overlay">Select item to see Chart.</span>
+          </div>
+        ) : null
+      }
+      headlineComponent={
+        <div className="live-select-wrap">
+          <Select
+            value={pickedSuggest}
+            onChange={selectedOption => setPickedSuggest(selectedOption)}
+            options={options}
+          />
+        </div>
+      }
+    />
   );
 }
-export default Device;
+export default Chart;
