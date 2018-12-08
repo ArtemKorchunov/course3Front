a8447b910c53303a2c1675d72859e1a8364b5bf5
ArtemK <artem.korchunof@gmail.com>
2018-12-08 17:47:01 +0200

    add docker

--- src/services/API/getRootUrl.js
+++ src/services/API/getRootUrl.js
@@ -1,3 +1,3 @@
 export default function getRootUrl() {
-  return process.env.REACT_APP_API_URL || "http://localhost:3000/";
+  return process.env.REACT_API_URL  || process.env.REACT_APP_API_URL || "http://localhost:3000/";
 }
