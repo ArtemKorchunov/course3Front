7cadafa78ca15b9bf64cc3313c335d98a1898c69
Artem Korchunov <artem.korchunof@gmail.com>
2019-01-04 22:35:37 +0200

    fix

--- src/services/API/getRootUrl.js
+++ src/services/API/getRootUrl.js
@@ -1,7 +1,3 @@
 export default function getRootUrl() {
-  return (
-    process.env.REACT_API_URL ||
-    process.env.REACT_APP_API_URL ||
-    "https://course3.herokuapp.com/"
-  );
+  return "https://course3.herokuapp.com/";
 }
